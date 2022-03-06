55645205744b7acaea4c318e65f84011d408ee11
icot <icot@users.noreply.github.com>
2022-03-06 11:05:30 +0100

    First commit

---
+++ src/org/traf/jlox/Main.java
@@ -0,0 +1,11 @@
+package org.traf.jlox;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Starting up");
+        Lox.main(args);
+
+    }
+}
